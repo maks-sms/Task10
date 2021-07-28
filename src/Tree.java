08f1f5dbeabb04b57662030a0b8da24a4b037348
Maks <maks988ss@gmail.com>
2021-07-28 23:21:34 +0300

    fix

--- src/Tree.java
+++ src/Tree.java
@@ -1,40 +1,40 @@
 public class Tree {
-        Node root = new Node(-1);
+        Node root;
         boolean addToLeft = true;
+
+        Tree(int value) {
+            root = new Node(value);
+        }
 
         public void add(int value) {
             addRecursive(root, value);
         }
 
-        private void addRecursive(Node node, int value) {
-            if (node.left == null) {
-                node.left = new Node(value);
-                return;
-            }
-            if (node.right == null) {
-                node.right = new Node(value);
-                return;
-            }
-            if (addToLeft) {
-                addToLeft = false;
-                addRecursive(node.left, value);
-            } else {
-                addToLeft = true;
-                addRecursive(node.right, value);
-            }
+    private Node addRecursive(Node node, int value) {
+        if(node == null) {
+            return new Node(value);
         }
+        if(value < node.value) {
+            node.left = addRecursive(node.left, value);
+        }
+        if(value > node.value) {
+            node.right = addRecursive(node.right, value);
+        }
+        return node;
+    }
 
         public void print() {
             printRecursive(root);
         }
 
         private void printRecursive(Node node) {
-            System.out.println(node.value);
             if (node.left != null) {
                 printRecursive(node.left);
             }
+            System.out.println(node.value);
             if (node.right != null) {
                 printRecursive(node.right);
             }
+
         }
     }
